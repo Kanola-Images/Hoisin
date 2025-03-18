e2da92b17795a28b3b547db029867f15959f1620
Max MacNeill <d3n2c@unb.ca>
2025-03-18 08:21:38 -0300

    Revert "Update first setup check script to use correct commands"
    This reverts commit e4dddeb8ddb21f8b76a8f3717071d2bc4fbca4d7.

--- includes.container/etc/greetd/checkforvanilla.sh
+++ includes.container/etc/greetd/checkforvanilla.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if id "vanilla" > /dev/null 2>&1; then
-    su -c "Hyprland --config /etc/greetd/firstsetup.conf" -s /bin/bash vanilla
+    su -C "Hyprland --config /etc/greetd/firstsetup.conf" -s /bin/bash vanilla
 else
-    su -c "Hyprland --config /etc/greetd/hyprland.conf" -s /bin/bash _greetd
+    Hyprland --config /etc/greetd/hyprland.conf
 fi
