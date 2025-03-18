3f9f0de3b19bf6d605f0ee04a549b2551d7b5db3
Max MacNeill <d3n2c@unb.ca>
2025-03-18 08:21:42 -0300

    Revert "try a thing with greetd"
    This reverts commit b35f4f4413555cd3601600e1cd28b9aba2356cb9.

--- includes.container/etc/greetd/checkforvanilla.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if id "vanilla" > /dev/null 2>&1; then
-    su -C "Hyprland --config /etc/greetd/firstsetup.conf" -s /bin/bash vanilla
-else
-    Hyprland --config /etc/greetd/hyprland.conf
-fi
